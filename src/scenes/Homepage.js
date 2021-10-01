0331714640b0ad217766b7fbcb8d73d82d5e5525
ahmadm712 <ahmadmuji2808@gmail.com>
2021-10-01 11:49:59 +0700

    menambahkan section

--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,14 +1,47 @@
 import React from "react";
-import Button from "elements/Button";
+import Section from "elements/Section";
 import Header from "parts/Header";
+import HeroImage from "assets/images/hero-image.png";
+import Zoom from "react-reveal/Zoom";
 
 export default function Homepage() {
   return (
-    <div>
-      <Header>
+    <div className="body-wrap">
+      <Header></Header>
+      <main className="site-content">
+        <Section className="hero illustration-section-01" isCenterContent>
+          <div className="container-sm">
+            <div className="hero-inner section-inner">
+              <div className="hero-content">
+                <Zoom bottom delay={500}>
+                  <h1 className="mt-0 mb-16">Landing template for startups</h1>
+                </Zoom>
+                <Zoom bottom delay={1000}>
+                  <p className="mt-0 mb-32">
+                    Our landing page template works on all devices, so you only
+                    have to set it up once, and get beautiful results forever.
+                  </p>
+                </Zoom>
 
-      </Header>
-      
+                {/* <div className="container-xs">
+                  
+                </div> */}
+              </div>
+              <div className="hero-figure illustration-element-01">
+                <Zoom bottom delay={1500}>
+                  <img
+                    className="has-shadow"
+                    src={HeroImage}
+                    alt="Hero"
+                    width="896"
+                    height="504"
+                  />
+                </Zoom>
+              </div>
+            </div>
+          </div>
+        </Section>
+      </main>
     </div>
   );
 }
