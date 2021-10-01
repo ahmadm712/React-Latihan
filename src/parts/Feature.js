74f999aa08afa0da0610fe59b9c5d57844a06cc8
ahmadm712 <ahmadmuji2808@gmail.com>
2021-10-02 00:33:10 +0700

    menambahkan feature v8

--- src/parts/Feature.js
+++ src/parts/Feature.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+
+import PropTypes from "prop-types";
+import Zoom from "react-reveal/Zoom";
 
 export default function Feature(props) {
-    return (
-        <div>
-            
+  return (
+    <div className="tiles-item">
+      <Zoom bottom delay={props.delayInMs}>
+        <div className="tiles-item-inner">
+          <div className="features-tiles-item-header">
+            <div className="features-tiles-item-image mb-16">
+              <img
+                src={props.data.imgSrc}
+                alt={props.data.imgAlt}
+                width="64"
+                height="64"
+              />
+            </div>
+          </div>
+          <div className="features-tiles-item-content">
+            <h4 className="mt-0 mb-8">{props.data.title}</h4>
+            <p className="m-0 text-sm">
+            {props.data.description}
+            </p>
+          </div>
         </div>
-    )
+      </Zoom>
+    </div>
+  );
 }
+
+Feature.PropTypes = {
+    delayInMs:PropTypes.number,
+    data :PropTypes.object
+};
--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -2,11 +2,38 @@ import React from "react";
 import Section from "elements/Section";
 import Header from "parts/Header";
 import HeroImage from "assets/images/hero-image.png";
+import FeatureTile01 from "assets/images/feature-tile-icon-01.svg";
+import FeatureTile02 from "assets/images/feature-tile-icon-02.svg";
+import FeatureTile03 from "assets/images/feature-tile-icon-03.svg";
+
 import Zoom from "react-reveal/Zoom";
 import Clients from "parts/Clients";
-
+import Feature from "parts/Feature";
 
 export default function Homepage() {
+  const features = [
+    {
+      imgSrc: FeatureTile01,
+      imgAlt: "Feature tile icon 01",
+      title: "Join the system",
+      description:
+        "A pseudo-Latin text used in webdesign, layout, and printing inplace of things to emphasise design.",
+    },
+    {
+      imgSrc: FeatureTile02,
+      imgAlt: "Feature tile icon 02",
+      title: "Join the system",
+      description:
+        "A pseudo-Latin text used in webdesign, layout, and printing inplace of things to emphasise design.",
+    },
+    {
+      imgSrc: FeatureTile03,
+      imgAlt: "Feature tile icon 03",
+      title: "Join the system",
+      description:
+        "A pseudo-Latin text used in webdesign, layout, and printing inplace of things to emphasise design.",
+    },
+  ];
   return (
     <div className="body-wrap">
       <Header></Header>
@@ -42,9 +69,22 @@ export default function Homepage() {
               </div>
             </div>
           </div>
-          <Clients>
-            
-          </Clients>
+          <Clients></Clients>
+        </Section>
+        <Section className="features-tiles">
+          <div className="container">
+            <div className="features-tiles-inner section-inner">
+              <div className="tiles-wrap">
+                {features.map((features, index) => (
+                  <Feature
+                    key={index}
+                    delayInMS={index*500}
+                    data={features}
+                  ></Feature>
+                ))}
+              </div>
+            </div>
+          </div>
         </Section>
       </main>
     </div>
